bc4a3fc07a99b4a10567c313854af7b61806b4a3
Janga Venu Gopal <jvg70752@gmail.com>
2026-07-08 20:32:45 +0530

    Add AWS provider configuration
    Add AWS provider configuration with mock credentials.

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_key"
+  secret_key                  = "mock_secret"
+}
+
 # infra/envs/prod/main.tf
 
 module "network" {
