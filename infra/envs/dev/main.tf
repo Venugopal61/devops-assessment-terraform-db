38bff10c9edf207c38886e78a726f5c9224927d0
Janga Venu Gopal <jvg70752@gmail.com>
2026-07-08 20:31:06 +0530

    Configure AWS provider with mock credentials
    Added AWS provider configuration with dummy credentials for development.

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,5 +1,15 @@
-# infra/envs/dev/main.tf
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 
+  # Dummy credentials to bypass your expired account checks
+  access_key = "mock_key"
+  secret_key = "mock_secret"
+}
+
+# infra/envs/dev/main.tf
 
 module "network" {
   source = "../../modules/network"
